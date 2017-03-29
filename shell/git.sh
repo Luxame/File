0602ed245fb2cb6900c65c83dbcfb09349604475
Luxame <asd869zxc@hotmail.com.tw>
2017-03-29 16:34:11 +0800

    upload

--- shell/git.sh
+++ shell/git.sh
@@ -8,3 +8,4 @@ git config --global user.name "UserName"
 git config --global color.ui true
 git config --global core.editor vim
 git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --list --global
