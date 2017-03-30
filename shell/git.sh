03c82dc5e98f8e036e3fb813ce1a6042219ef0d4
Luxame <asd869zxc@hotmail.com.tw>
2017-03-30 11:36:27 +0800

    更新 git.sh

--- shell/git.sh
+++ shell/git.sh
@@ -4,12 +4,16 @@
 #有需要安裝 Git 請把最前面的 '#' 拿掉 
 # yum install git -y
 # apt-get install git -y
+
 # Test
 git
+
 # Git config
 git config --global user.email "Mail"
 git config --global user.name "UserName"
 git config --global color.ui true
 git config --global core.editor vim
 git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+# 查看 Git 設定檔
 git config --list --global
