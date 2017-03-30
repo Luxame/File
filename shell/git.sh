eb768442255346b0f50c43acb995ff5379445fdd
Luxame <asd869zxc@hotmail.com.tw>
2017-03-30 10:50:54 +0800

    更新 git.sh

--- shell/git.sh
+++ shell/git.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# test
+# Install Git
+#有需要安裝 Git 請把最前面的 '#' 拿掉 
+# yum install git -y
+# apt-get install git -y
+# Test
 git
-# git config
+# Git config
 git config --global user.email "Mail"
 git config --global user.name "UserName"
 git config --global color.ui true
