8da5d47912e1942252d56aee61e75fd2cf3ec94d
Luxame <asd869zxc@hotmail.com.tw>
2020-05-03 00:41:39 +0800

    add new script for download tomcat

---
+++ shell/auto-download-tomcat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# auto download latest tomcat tar file
+# and setting somethings
+
+read -p "Download Latest Tomcat Tar File? Y/N :" ask
+
+if [ $ask = N ]
+then
+    exit 0;
+elif [ $ask = Y ]
+then
+    read -p "Version :" version
+    tomcat_version=v${version}
+    curl http://ftp.mirror.tw/pub/apache/tomcat/tomcat-8/${tomcat_version}/bin/apache-tomcat-${version}.tar.gz -o apache-tomcat-${version}.tar.gz
+fi
