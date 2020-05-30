c729c83ef23e0f79454fe70b035f1ce4e1fdc5ea
Luxame <asd869zxc@hotmail.com.tw>
2020-05-31 00:43:16 +0800

    update auto-download-tomcat

--- shell/auto-download-tomcat.sh
+++ shell/auto-download-tomcat.sh
@@ -14,3 +14,15 @@ then
     tomcat_version=v${version}
     curl http://ftp.mirror.tw/pub/apache/tomcat/tomcat-8/${tomcat_version}/bin/apache-tomcat-${version}.tar.gz -o apache-tomcat-${version}.tar.gz
 fi
+
+read -p "Need create new tomcat? Y/N :" ask
+
+if [ $ask = N ]
+then
+    exit 0;
+elif [ $ask = Y ]
+then
+    tar -xzf apache-tomcat-${version}.tar.gz
+    read -p "Service Name :" tomcat_name
+    mv apache-tomcat-${version} $tomcat_name
+fi
