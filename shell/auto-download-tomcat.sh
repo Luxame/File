a4bb29527a4b3f5f8f05162b014da3f44cd5698e
Luxame <asd869zxc@hotmail.com.tw>
2020-05-31 17:58:49 +0800

    update auto-download-tomcat - add check user script

--- shell/auto-download-tomcat.sh
+++ shell/auto-download-tomcat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# auto download latest tomcat 8.5.X tar file
-# and setting somethings
+# auto download latest tomcat 8.5.X tar file and setting somethings.
+# the script have some command need root permission,
+# advise use have sudo permission or root user to run the script.
+
+# linux command variable
 
 location=$(pwd)
+
+# Run script
 
 read -p "Download Latest Tomcat Tar File? Y/N :" ask
 
@@ -39,4 +44,18 @@ elif [ $ask = Y ]
 then
     read -p "Please enter directory location :" tomcat_local
     mv $tomcat_name $tomcat_local
+fi
+
+echo "Check tomcat user have exist"
+user_tomcat=$(id tomcat)
+
+if [ $? -eq 0]
+then
+    echo "Tomcat user is exist"
+    exit 0;
+elif [ $? -eq 1]
+    echo "Will create tomcat user"
+    useradd -d /opt/tomcat -s /sbin/nologin tomcat
+    echo "re-check user, run linux command"
+    id tomcat
 fi
