5f48adc2cf0e2fe61de96a87b531beafcd104841
Luxame <asd869zxc@hotmail.com.tw>
2021-03-27 21:22:15 +0800

    update backup-bookstack

--- shell/bookstack-backup.sh
+++ shell/bookstack-backup.sh
@@ -3,11 +3,19 @@
 dirTime=$(date +"%Y%m%d")
 fileTime=$(date +"%Y%m%d%H%M")
 
+## 建立日期資料夾
 mkdir -p /opt/backup/$dirTime
 
+## 備份檔案
 mysqldump --no-tablespaces --user=bookstack --password=XXXXXXXX bookstack > /opt/backup/$dirTime/$fileTime.sql
 cp -rp /var/www/bookstack/.env /opt/backup/$dirTime/env-$fileTime
 cp -rp /var/www/bookstack/storage/uploads /opt/backup/$dirTime/storage-$dirTime
 cp -rp /var/www/bookstack/public/uploads /opt/backup/$dirTime/public-$dirTime
+
+## 壓縮
 cd /opt/backup/
 tar -zcf $dirTime.tar.gz $dirTime
+
+## 刪除過去備份
+find /opt/backup/ -type d -mtime +2 > old.txt
+rm -r $( < old.txt )
