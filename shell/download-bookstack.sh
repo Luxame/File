91b8f07b4e4eac926fe90e02f94fd99d8ace9654
Luxame <asd869zxc@hotmail.com.tw>
2021-04-16 18:43:05 +0800

    update download-bookstack script

--- shell/download-bookstack.sh
+++ shell/download-bookstack.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# use rsync to download file
 rsync -azP --delete ssh_config_name:/opt/backup/*.tar.gz dest_dir
+
+# use find to delete old file
+find bacupfile_dest -type f -mtime +2 -exec rm {} \;
